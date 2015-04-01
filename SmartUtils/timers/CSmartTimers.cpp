9e831fabefa828e975d661c790c9bcd8b26b0793
rock <rock@localhost.localdomain>
2015-04-01 16:29:10 +0800

    ok

---
+++ SmartUtils/timers/CSmartTimers.cpp
@@ -0,0 +1,21 @@
+/*
+ * CSmartTimers.cpp
+ *
+ *  Created on: Apr 1, 2015
+ *      Author: rock
+ */
+
+#include "CSmartTimers.h"
+
+namespace ns_utils {
+
+CSmartTimers::CSmartTimers() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CSmartTimers::~CSmartTimers() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ns_utils */
---
+++ SmartUtils/timers/CSmartTimers.h
@@ -0,0 +1,55 @@
+/*
+ * CSmartTimers.h
+ *
+ *  Created on: Apr 1, 2015
+ *      Author: rock
+ */
+
+#ifndef CSMARTTIMERS_H_
+#define CSMARTTIMERS_H_
+
+
+#include <cstdint>
+#include <memory>
+
+namespace ns_utils {
+
+class CTimerHandler{
+protected:
+	CTimerHandler(){}
+	virtual ~CTimerHandler(){}
+
+public:
+	virtual void_t handle_timer_evt(uint64_t ui64Times) = 0;
+
+};
+typedef std::shared_ptr<ns_utils::CTimerHandler> TimerHandlerPtr_t;
+
+
+class CSmartTimers {
+
+public:
+	enum EClockType
+	{
+		ECT_BASE = 0,
+		ECT_REALTIME = ECT_BASE + 1,
+		ECT_MONOTONIC = ECT_BASE + 2;
+	};
+
+public:
+	CSmartTimers();
+	virtual ~CSmartTimers();
+
+public:
+	int32_t start();
+	int32_t stop();
+
+	int32_t add_timer();
+	int32_t remove_timer();
+
+
+};
+
+} /* namespace ns_utils */
+
+#endif /* CSMARTTIMERS_H_ */
