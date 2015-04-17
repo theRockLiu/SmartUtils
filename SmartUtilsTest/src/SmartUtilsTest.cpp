d64c127760bb7216a8c9030e775ac0a77f48fa56
rock <rock@localhost.localdomain>
2015-04-17 12:53:01 -0400

    www

--- SmartUtilsTest/src/SmartUtilsTest.cpp
+++ SmartUtilsTest/src/SmartUtilsTest.cpp
@@ -23,16 +23,28 @@ public:
 
 int main()
 {
-	ns_utils::timer_ptr_t ptr = std::make_shared < CMyTimerHandler > ();
+	ns_utils::timer_ptr_t ptr = std::make_shared<CMyTimerHandler>();
 
 	ns_utils::CSmartTimers tms;
 	tms.start();
+	ptr->set_timer_type(ns_utils::EClockType::ECT_REALTIME);
+	ptr->set_init_expire_time(1, 1);
+	ptr->set_interval_time(1, 1);
+
+	cout <<"sss: " <<ptr.use_count() << endl; // prints !!!Hello World!!!
 	tms.add_timer(ptr);
 
-    std::this_thread::sleep_for(100s);
+	cout <<"sss: " <<ptr.use_count() << endl; // prints !!!Hello World!!!
 
-    tms.stop();
+	std::this_thread::sleep_for(10s);
 
+	// ptr;
+
+	cout << "!!!stop!!!" << endl; // prints !!!Hello World!!!
+
+	std::this_thread::sleep_for(10s);
+
+	tms.stop();
 
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
